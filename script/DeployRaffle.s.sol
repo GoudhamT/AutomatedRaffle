92e5f68b5aad0652341a04a7a810b3cf8c99d4c0
GoudhamT <goudhamvel.88@gmail.com>
2025-08-27 22:19:25 -0700

    Script is implemented

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -2,8 +2,27 @@
 
 pragma solidity 0.8.19;
 import {Script} from "forge-std/Script.sol";
+import {AutomatedRaffle} from "src/AutomatedRaffle.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
 
 contract DeployRaffle is Script {
+    // AutomatedRaffle public automatedRaffle;
+    HelperConfig helperConfig;
 
-    fcuntion run() public {}
+    function run() public {}
+
+    function deployContract() public returns (AutomatedRaffle, HelperConfig) {
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+        vm.startBroadcast();
+        AutomatedRaffle automatedRaffle = new AutomatedRaffle(
+            config.enteranceFee,
+            config.keyHash,
+            config.subscriptionId,
+            config.gasLimit,
+            config.vrfCoordinator
+        );
+        vm.stopBroadcast();
+
+        return (automatedRaffle, helperConfig);
+    }
 }
